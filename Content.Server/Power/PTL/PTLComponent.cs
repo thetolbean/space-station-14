42b4d798f93bcd2086256d0e48d526fd4977499a
Preston Smith <blackfoot03@outlook.com>
2024-12-05 16:26:14 -0600

    Start work on PTL

---
+++ Content.Server/Power/PTL/PTLComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Server.power.PTL
+{
+    /// <summary>
+    /// Absorbs power up to its capacity when anchored then explodes.
+    /// </summary>
+    [RegisterComponent, AutoGenerateComponentPause]
+    public sealed partial class PtlComponent : Component
+    {
+
+    }
+}
---
+++ Content.Server/Power/PTL/PTLSystem.cs
@@ -0,0 +1,26 @@
+﻿namespace Content.Server.Power.PTL;
+
+
+public sealed class PtlSystem : EntitySystem
+{
+    // Function from file CargoSystem.Orders.cs
+    /* This function updates the station Bank Account, which we will need
+    private void OnInteractUsing(EntityUid uid, CargoOrderConsoleComponent component, ref InteractUsingEvent args)
+    {
+        if (!HasComp<CashComponent>(args.Used))
+            return;
+
+        var price = _pricing.GetPrice(args.Used);
+
+        if (price == 0)
+            return;
+
+        var stationUid = _station.GetOwningStation(args.Used);
+
+        if (!TryComp(stationUid, out StationBankAccountComponent? bank))
+            return;
+
+        UpdateBankAccount(stationUid.Value, bank, (int) price);
+        QueueDel(args.Used);
+    } */
+}
