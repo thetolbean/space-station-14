60cbb3b7fcbd5a91a064a0147fbb1bc60f5c6f19
Preston Smith <blackfoot03@outlook.com>
2024-12-05 20:30:39 -0600

    Make PTL drain and give money

--- Content.Server/Power/PTL/PTLComponent.cs
+++ Content.Server/Power/PTL/PTLComponent.cs
@@ -1,11 +1,49 @@
 ﻿namespace Content.Server.power.PTL
 {
     /// <summary>
-    /// Absorbs power up to its capacity when anchored then explodes.
+    /// Stores power and then transfers it via bluespace to other stations.
     /// </summary>
-    [RegisterComponent, AutoGenerateComponentPause]
+    [RegisterComponent]
     public sealed partial class PtlComponent : Component
     {
+        /// <summary>
+        /// If the PTL is currently set to sell power.
+        /// </summary>
+        [ViewVariables]
+        public bool Enabled = true;
 
+        /// <summary>
+        /// The amount of power (in watts) to draw when turned on
+        /// </summary>
+        [DataField("powerDraw")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public int PowerDraw = 1000;
+
+        /// <summary>
+        /// The amount of power the particle accelerator must be provided with relative to the expected power draw to function.
+        /// </summary>
+        [ViewVariables]
+        public const float RequiredPowerRatio = 0.999f;
+
+        /// <summary>
+        /// The amount of time between
+        /// </summary>
+        [DataField("sellTime")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public TimeSpan SellTime = TimeSpan.FromSeconds(1.0);
+
+        /// <summary>
+        /// The time at which the PA last fired a wave of particles.
+        /// </summary>
+        [DataField("lastSell")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public TimeSpan LastSell;
+
+        /// <summary>
+        /// The time at which the PA will next fire a wave of particles.
+        /// </summary>
+        [DataField("nextSell")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public TimeSpan NextSell;
     }
 }
--- Content.Server/Power/PTL/PTLSystem.cs
+++ Content.Server/Power/PTL/PTLSystem.cs
@@ -1,26 +1,55 @@
-﻿namespace Content.Server.Power.PTL;
+﻿using Content.Server.Cargo.Components;
+using Content.Server.Station.Systems;
+using Content.Shared.FixedPoint;
+using Content.Server.Cargo.Systems;
+using Content.Server.Power.Components;
+using Content.Server.power.PTL;
+using Robust.Shared.Timing;
+
+namespace Content.Server.Power.PTL;
 
 
 public sealed class PtlSystem : EntitySystem
 {
-    // Function from file CargoSystem.Orders.cs
-    /* This function updates the station Bank Account, which we will need
-    private void OnInteractUsing(EntityUid uid, CargoOrderConsoleComponent component, ref InteractUsingEvent args)
+    [Dependency] private readonly StationSystem _station = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly CargoSystem _cargo = default!;
+
+    public override void Update(float frameTime)
     {
-        if (!HasComp<CashComponent>(args.Used))
+        var curTime = _gameTiming.CurTime;
+        var query = EntityQueryEnumerator<PtlComponent>();
+        while (query.MoveNext(out var uid, out var ptl))
+        {
+            if (ptl.Enabled && curTime >= ptl.NextSell)
+            {
+                PtlUpdateBank(uid, ptl, curTime, 100);
+            }
+        }
+    }
+
+
+    private void PtlUpdateBank(EntityUid uid, PtlComponent ptl, TimeSpan curTime, int price)
+    {
+        if (price <= FixedPoint2.Zero)
             return;
 
-        var price = _pricing.GetPrice(args.Used);
-
-        if (price == 0)
+        if (!(TryComp<PowerConsumerComponent>(uid, out var powerConsumer)))
             return;
 
-        var stationUid = _station.GetOwningStation(args.Used);
+        if (powerConsumer.NetworkLoad.ReceivingPower < powerConsumer.NetworkLoad.DesiredPower)
+            return;
+
+        var stationUid = _station.GetOwningStation(uid);
 
         if (!TryComp(stationUid, out StationBankAccountComponent? bank))
             return;
 
-        UpdateBankAccount(stationUid.Value, bank, (int) price);
-        QueueDel(args.Used);
-    } */
+        ptl.LastSell = curTime;
+        ptl.NextSell = curTime + ptl.SellTime;
+
+        _cargo.UpdateBankAccount(stationUid.Value, bank, price);
+    }
+
+
 }
